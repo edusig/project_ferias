839402375389a31b89949a6c68cad774c791cc25
Cainã D'Ajuda <caina@localhost.localdomain>
2015-08-07 20:58:28 -0300

    Add a new screen of file chooser
    The added file selection screen, but without increments design.
    
    Change-Id: I43b1b4e7fd68115c660fb344cda211ebb3d1fab6

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 __version__ = '0.0.1'
-import kivy
 
 from kivy import properties
 from kivy.app import App
@@ -7,6 +6,9 @@ from kivy.uix.boxlayout import BoxLayout
 #from kivy.uix import settings
 #from kivy.uix.widget import Widget
 from kivy.lang import Builder
+
+# XXX: This import is related to fullscreen
+#from kivy.config import Config
 
 import source_screen, type_selector, status_bar
 
@@ -16,6 +18,7 @@ class Main(BoxLayout):
     def __init__(self, **kwargs):
         super(Main, self).__init__(**kwargs)
         self.ids.screen_manager.add_widget(source_screen.Source_Screen(name='source_screen'))
+        self.ids.screen_manager.add_widget(type_selector.Type_Selector(name='file_chooser'))
 
 
 class PresenterApp(App):
@@ -26,20 +29,19 @@ class PresenterApp(App):
     #   Properties
     #
 
-    title = properties.StringProperty('Presenter')
+    title = properties.StringProperty('Opuntia')
 
     #
     #   App implementation
     #
 
     def build(self):
+        # This line activated the full screen mode
+        #Config.set('graphics', 'fullscreen', 'auto')
+        self.icon = 'data/pixmaps/icon.png'
         Builder.load_file('visual.kv')
         root = Main()
         return root
-
-    #
-    #   Callbacks
-    #
 
 
 if __name__ == '__main__':
--- type_selector.py
+++ type_selector.py
@@ -1,6 +1,85 @@
+import os
+import subprocess
+
 from kivy import properties
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen
 
 
-class Type_Selector(BoxLayout):
+class Type_Selector(Screen):
     app = properties.ObjectProperty()
+
+    extensions = {
+        'presentation': ['.ppt',
+                         '.pptx',
+                         '.pdf',
+                         '.odp'],
+
+        'music': ['.mp3'],
+
+        'video': ['.mp4',
+                  '.avi'],
+
+        'image': ['.jpg',
+                  '.jpeg',
+                  '.bmp',
+                  '.png'],
+    }
+
+    #
+    #   Public
+    #
+
+    def filter_selected(self, filter_type=['*']):
+        del self.ids.file_chooser.filters[:]
+        for i in filter_type:
+            self.ids.file_chooser.filters.append(i)
+
+    def go_back(self):
+        self.manager.current = 'source_screen'
+
+    def load_file(self, filename):
+        path = str(filename.pop())
+        name, extension = os.path.splitext(path)
+        if extension in self.extensions['presentation']:
+            self._load_presenter(path, extension)
+            return
+
+        if extension in self.extensions['music']:
+            self._load_music(path)
+            return
+
+        if extension in self.extensions['video']:
+            self._load_video(path)
+            return
+
+        if extension in self.extensions['image']:
+            self._load_image(path)
+            return
+
+    def preview_file(self, filename):
+        if len(filename) is 0:
+            return None
+        return filename[0]
+
+    #
+    #   Private
+    #
+
+    # XXX: Quando os testes forem feitos no RPi eh necessarios listar e alterar os
+    #leitores de formatos para sua utilizacao.
+    def _load_image(self, filename):
+        subprocess.call(['eog', '-f', filename], shell=False)
+
+    def _load_music(self, filename):
+        subprocess.call(['totem', filename], shell=False)
+
+    def _load_presenter(self, filename, extension):
+        print type(extension)
+        if extension == '.pdf':
+            subprocess.call(['xpdf', '-fullscreen', filename], shell=False)
+            return
+
+        subprocess.call(['libreoffice', '--nologo', '--show', filename], shell=False)
+
+    def _load_video(self, filename):
+        subprocess.call(['totem', filename], shell=False)
